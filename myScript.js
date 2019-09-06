fee799e6c1f920be4f4de46398244d26e38569da
Katia Isagawa <katty_ka@hotmail.com>
2019-09-06 10:34:30 +1000

    Initial commit

--- myScript.js
+++ myScript.js
@@ -1,7 +1,8 @@
-﻿$("#hide").click(function(){
-  $("p").hide();
-});
-
-$("#show").click(function(){
-  $("p").show();
+﻿$(document).ready(function(){
+  $("#hide").click(function(){
+    $("p").hide();
+  });
+  $("#show").click(function(){
+    $("p").show();
+  });
 });
